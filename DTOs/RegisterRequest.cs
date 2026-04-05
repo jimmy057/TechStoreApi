49d6aee014a82e52d3c4e867c7fc5cc1509a94cb
jimmy057 <jimmypolanco2540@gmail.com>
2026-04-05 16:58:44 -0400

    modificando y agregando register reguest

---
+++ DTOs/RegisterRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace TechStoreApi.DTOs
+{
+	public class RegisterRequest
+	{
+		public string Nombre { get; set; } = string.Empty;
+		public string Email { get; set; } = string.Empty;
+		public string Clave { get; set; } = string.Empty; 
+	}
+}
--- DTOs/UsuarioDTO.cs
+++ DTOs/UsuarioDTO.cs
@@ -2,9 +2,9 @@
 {
 	public class UsuarioDTO
 	{
-		public int Id { get; set; }
-		public string NombreCompleto { get; set; } = string.Empty;
+		public int UsuarioId { get; set; }    
+		public string Nombre { get; set; } = string.Empty; 
 		public string Email { get; set; } = string.Empty;
-		public string Token { get; set; } = string.Empty; 
+		public string Token { get; set; } = string.Empty;
 	}
 }
